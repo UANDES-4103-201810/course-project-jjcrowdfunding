3e5d1fb426de33a083088bafeb61a518cd2f0435
juanesgh <jerodriguez@miuandes.cl>
2018-04-13 01:36:25 -0300

    validations users projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,25 @@
 class Project < ApplicationRecord
+	has_many :userratings
+	has_many :userwishlists
+	has_many :guestviewprojects
+	has_many :projectcomments
+	has_many :userprojects
+	has_many :userviewprojects
+	has_many :promises
+	has_many :projectcategories
+	has_many :users, through: :userratings
+	has_many :users, through: :userwishlists
+	has_many :guests, through: :guestviewprojects
+	has_many :users, through: :projectcomments
+	has_many :users, through: :userprojects
+	has_many :users, through: :userviewprojects
+	has_many :userpromises, through: :promises
+	has_many :categories, through: :projectcategories
+	validates :description, presence: true
+	validates :description, length: { in: 50..500}
+	validates :tittle, presence: true
+	validates :description, presence: true
+	validates :goal_amount, presence: true
+	validates :duedate, presence: true
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+	has_many :userprojects
+	has_many :userwishlists
+	has_many :projectcomments
+	has_many :userpromises
+	has_many :userratings
+	has_many :userviewprojects
+	has_many :projects, through: :projectcomments
+	has_many :projects, through: :userprojects
+	has_many :projects, through: :userwishlists
+	has_many :projects, through: :userratings
+	has_many :projects, through: :userviewprojects
+	validates :name, presence: true
+	validates :lastname, presence: true
+	validates :email, presence: true
+	validates :phone, presence: true
+	validates :password, presence: true
+	validates :password, length: { in: 8..25}
 end
