541f8db9cfb8f6357b1cf56341c896520d833c85
jauger77 <jiauger@miuandes.cl>
2018-05-24 11:33:29 -0400

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get 'home/show'
 
   resources :projects, :users
-  root 'users#show'
+  root 'projects#index'
   
-  get '/profile', to: 'application#profile', as: 'profile'
+  get '/users/:id', to: 'users#show', as: 'profile'
   get '/register', to: 'application#register', as: 'register'
   get '/search', to: 'application#search', as: 'search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
