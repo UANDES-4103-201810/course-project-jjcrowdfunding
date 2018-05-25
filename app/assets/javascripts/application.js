b27d70692e20f63d6fe634fba2f4d6cac318fc87
jauger77 <jiauger@miuandes.cl>
2018-05-25 19:20:01 -0400

    final bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery?ujs
+//= require jquery-ujs
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require turbolinks
