e7b18e8338477e0c44e514b893146fc85dab28cb
juanesgh <jerodriguez@miuandes.cl>
2018-05-25 22:21:30 -0400

    User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180524232953) do
+ActiveRecord::Schema.define(version: 20180526021540) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -136,6 +136,11 @@ ActiveRecord::Schema.define(version: 20180524232953) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "lastname"
+    t.integer "phone"
+    t.text "description"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
